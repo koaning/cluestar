31c99e2f07a932c606a5a629d03b1b003e638a77
Vincent <vincent@calmcode.io>
2023-10-26 16:11:18 +0200

    maybe check for a bit more

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -7,7 +7,7 @@ from cluestar import plot_text, plot_text_comparison
 def test_smoke_plot_text(s):
     texts = ["random {i}" for i in range(s)]
     X = np.random.normal(0, 1, (s, 2))
-    plot_text(X, texts)
+    assert texts[0] in plot_text(X, texts).to_json()
 
 
 @pytest.mark.parametrize("s", [500, 1000, 2000])
@@ -15,4 +15,4 @@ def test_smoke_plot_text_comparison(s):
     texts = ["random {i}" for i in range(s)]
     X1 = np.random.normal(0, 1, (s, 2))
     X2 = np.random.normal(0, 1, (s, 2))
-    plot_text_comparison(X1, X2, texts)
+    assert texts[0] in plot_text_comparison(X1, X2, texts).to_json()
